4b941c094e21c4a5ecd4ce0286ac15d6d1f33437
jason00111 <emberleyjason@gmail.com>
2016-12-08 09:29:36 -0800

    Cloud changes color

--- src/components/TestWidget.js
+++ src/components/TestWidget.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import '../styles/test_widget.css'
 
 class TestWidget extends Component {
   render() {
+    let className =
+      this.props.cloudColor === 'green' ?
+      'green-cloud' :
+      'blue-cloud'
+
     return (
       <div className="test-widget">
         This is a test widget that has state. See, click on this cloud ->
-        <span onClick={this.props.toggleCloud} className="green-cloud">{'\u2601'}</span>
+        <span onClick={this.props.toggleCloud} className={className}>{'\u2601'}</span>
       </div>
     )
   }
 }
 
-export default TestWidget
+export default connect(state => state)(TestWidget)
